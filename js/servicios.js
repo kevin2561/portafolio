15d5f66c240da4e03e5df04b4e9b0268d81896cc
dante <kevinnoa02@gmail.com>
2025-03-14 21:45:11 -0500

    reformado

---
+++ js/servicios.js
@@ -0,0 +1,14 @@
+export const leerJsonServicios = async (url, funcionHTML) => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: No se pudo cargar el JSON`);
+        }
+        const data = await response.json();
+        funcionHTML(data);
+        console.log("Datos cargados correctamente:", data);
+    } catch (error) {
+        console.error("Error al cargar el JSON:", error);
+    }
+};
+// https://codesandbox.io/p/sandbox/yl6ddq?file=%2Findex.html%3A10%2C1-11%2C1
